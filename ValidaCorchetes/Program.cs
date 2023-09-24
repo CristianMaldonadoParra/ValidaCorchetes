974e9ee1ba3a34a9f0b870a9382e29d9feaf3b1d
Freddy Parra <dbc.fcmparra@voegol.com.br>
2023-09-24 11:37:08 -0300

    Add project files.

---
+++ ValidaCorchetes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ValidaCorchetes
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Digite uma string contendo apenas colchetes:");
+            string input = Console.ReadLine();
+
+            try
+            {
+                if (VerificadorColchetes.ValidarEntrada(input))
+                {
+                    if (VerificadorColchetes.VerificarOrdemColchetes(input))
+                    {
+                        Console.WriteLine("A ordem dos colchetes é válida.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("A ordem dos colchetes não é válida.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("A entrada é inválida. Por favor, forneça uma string contendo apenas colchetes.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ ValidaCorchetes/VerificadorColchetes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidaCorchetes
+{
+    public class VerificadorColchetes
+    {
+        private static readonly Dictionary<char, char> mapaColchetes = new Dictionary<char, char>
+    {
+        { '(', ')' },
+        { '{', '}' },
+        { '[', ']' }
+    };
+
+        public static bool VerificarOrdemColchetes(string input)
+        {
+            Stack<char> pilha = new Stack<char>();
+
+            foreach (char c in input)
+            {
+                if (mapaColchetes.ContainsKey(c))
+                {
+                    pilha.Push(c);
+                }
+                else if (mapaColchetes.ContainsValue(c))
+                {
+                    if (pilha.Count == 0 || c != mapaColchetes[pilha.Pop()])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return pilha.Count == 0;
+        }
+
+        public static bool ValidarEntrada(string input)
+        {
+            return !string.IsNullOrWhiteSpace(input) && input.All(c => mapaColchetes.ContainsKey(c) || mapaColchetes.ContainsValue(c));
+        }
+    }
+}
